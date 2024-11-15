99f5e07a7614e17819ab76b465beb5795b4a0abc
Oussama Trabelsi <oussama.trabelsidev@gmail.com>
2024-11-15 01:18:12 +0100

    feat(domain): created product entity

---
+++ src/domain/entities/Product.ts
@@ -0,0 +1,13 @@
+export class Product {
+  constructor(
+    public readonly id: string,
+    public name: string,
+    public description: string,
+    public price: number,
+    public quantity: number,
+    public category: string,
+    public tags: string[],
+    public createdAt: Date,
+    public updatedAt: Date
+  ) {}
+}
