e30266e19093d0a192b6fe269f0f553133257707
Oussama Trabelsi <oussama.trabelsidev@gmail.com>
2024-11-15 16:13:36 +0100

    another one

--- src/infrastructure/database/mongodb-connection.ts
+++ src/infrastructure/database/mongodb-connection.ts
@@ -1,7 +1,5 @@
 import mongoose from 'mongoose';
 
-const RETRY_TIMEOUT = 5000;
-
 export class MongoDBConnection {
   private static _instance: MongoDBConnection;
 
@@ -12,66 +10,18 @@ export class MongoDBConnection {
   }
 
   public async connect() {
-    const mongoURI = process.env.MONGO_URI as string;
-
     try {
-      await this.retryConnection(mongoURI);
+      const mongoURI = process.env.MONGO_URI as string;
+      await mongoose.connect(mongoURI);
       console.log('MongoDB connected successfully.');
     } catch (err) {
-      console.error(
-        'Failed to connect to MongoDB after multiple attempts.',
-        err,
-      );
-      throw err;
+      console.error('MongoDB connection failed', err);
     }
   }
 
-  private async retryConnection(mongoURI: string, retries = 5): Promise<void> {
-    for (let attempt = 1; attempt <= retries; attempt++) {
-      try {
-        await mongoose.connect(mongoURI);
-        console.log('MongoDB connected successfully.');
-        return; // Exit on success
-      } catch (err) {
-        this.logConnectionError(err, attempt, retries);
-
-        if (attempt < retries) {
-          await this.delay(RETRY_TIMEOUT);
-        } else {
-          throw err; // Rethrow the error after the last attempt
-        }
-      }
-    }
-  }
-
-  private logConnectionError(
-    err: unknown,
-    attempt: number,
-    retries: number,
-  ): void {
-    const isLastAttempt = attempt === retries;
-    const message = isLastAttempt
-      ? `MongoDB connection failed after ${retries} attempts.`
-      : `MongoDB connection attempt ${attempt} failed. Retrying...`;
-
-    console.error(message, err);
-  }
-
-  private handleConnectionError(
-    err: unknown,
-    attempt: number,
-    isLastAttempt: boolean,
-  ): void {
-    if (isLastAttempt) {
-      console.error(`MongoDB connection failed after ${attempt} attempts.`);
-    } else {
-      console.error(
-        `MongoDB connection attempt ${attempt} failed. Retrying...`,
-      );
-    }
-  }
-
-  private delay(ms: number): Promise<void> {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+  static async close() {
+    await mongoose.disconnect();
+    // Handle graceful shutdown of DB connections
+    console.log('Disconnected from MongoDB');
   }
 }
